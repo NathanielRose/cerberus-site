5a42afd495ab92732f9fd1841e5adb5521642a02
Nate <NathanielRose@users.noreply.github.com>
2026-02-04 10:43:00 -0800

    Refactor subscribe.js to use ES module syntax

--- api/subscribe.js
+++ api/subscribe.js
@@ -1,8 +1,7 @@
 // api/subscribe.js
-module.exports = (req, res) => {
+export default function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
-
   return res.status(200).json({ ok: true });
-};
+}
